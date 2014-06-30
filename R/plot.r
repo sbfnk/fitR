a20c6aa284d924663026d7f7b606a4c2b99856b0
Anton <anton.camacho@lshtm.ac.uk>
2014-06-30 10:14:44 +0200

    Update plot functions
    All plot functions are based on plotTraj which allow to plot a summary
    of the trajectories (mean, median and CI).
    
    Still need to test some of them

--- R/plot.r
+++ R/plot.r
@@ -1,28 +1,82 @@
-#'Plot one or more model trajectories
+#'Plot one or more model simulations
 #'
-#'This function use faceting to plot all state.variables trajectories. Convenient to see results of several simulations.
-#' @param fitmodel a \code{\link{fitmodel}} object.
-#' @param traj data.frame, output of \code{fitmodel$simulate.model} or \code{simulateModelReplicates}.
-#' @param state.variables subset of state.variables to plot. If \code{NULL} (default) all state variables are plotted.
+#'This function use faceting to plot one or more trajectories of all state variables. Convenient to see results of several simulations. Also, if \code{data} is present, then an additional plot
+#' @param traj data.frame, output of \code{fitmodel$simulateTraj} or \code{simulateModelReplicates}.
+#' @param state.names character vector. Names of the state variables to plot. Names must match \code{fitmodel$state.names}. If \code{NULL} (default) all state variables are plotted.
+#' @param data data frame. Observation times and observed data. The time column must be named \code{time}, whereas the name of the data column should match one of \code{fitmodel$state.names}. 
+#' @param summary logical. If \code{TRUE}, the mean, median as well as the 50th and 95th percentile of the trajectories are plotted (default). If \code{FALSE}, all individual trajectories are plotted (transparency can be set with \code{alpha}).
 #' @param alpha transparency of the trajectories (between 0 and 1).
 #' @param plot if \code{TRUE} the plot is displayed, and returned otherwise.
 #' @export
 #' @import reshape2 ggplot2
-#' @seealso simulateModelReplicates
-plotModelTraj <- function(fitmodel,traj,state.variables=NULL,alpha=1, plot=TRUE) {
-
-    if(is.null(state.variables)){
-        state.variables <- fitmodel$state.variables
-    }
+#' @seealso \code{\link{simulateModelReplicates}}
+plotTraj <- function(traj, state.names=NULL, data=NULL, summary=TRUE, alpha=1, plot=TRUE) {
 
     if(!"replicate"%in%names(traj) && !any(duplicated(traj$time))){
         traj$replicate <- 1
+
+        if(summary){
+            message("Only 1 replicate to summarise: mean, median and CI of the trajectories won't be plotted.")
+            summary <- FALSE
+        }
     }
 
-    df <- melt(traj,measure.vars=state.variables)
+    if(is.null(state.names)){
+        state.names <- setdiff(names(traj),c("time","replicate"))
+    }
 
-    p <- ggplot(df,aes(x=time,y=value,group=replicate))+facet_wrap(~variable, scales="free_y")
-    p <- p + geom_line(alpha=alpha)
+    df.traj <- melt(traj,measure.vars=state.names,variable.name="state")
+
+    if(summary){
+
+        message("Compute confidence intervals")
+
+        traj.CI <- ddply(df.traj,c("time","state"),function(df) {
+
+            tmp <- as.data.frame(t(quantile(df$value,prob=c(0.025,0.25,0.5,0.75,0.975))))
+            names(tmp) <- c("low_95","low_50","median","up_50","up_95")
+            tmp$mean <- mean(df$value)
+            return(tmp)
+
+        },.progress="text")
+
+        traj.CI.line <- melt(traj.CI[c("time","state","mean","median")],id.vars=c("time","state"))
+        traj.CI.area <- melt(traj.CI[c("time","state","low_95","low_50","up_50","up_95")],id.vars=c("time","state"))
+        traj.CI.area$type <- sapply(traj.CI.area$variable,function(x) {str_split(x,"_")[[1]][1]})
+        traj.CI.area$CI <- sapply(traj.CI.area$variable,function(x) {str_split(x,"_")[[1]][2]})
+        traj.CI.area$variable <- NULL
+        traj.CI.area <- dcast(traj.CI.area,"time+state+CI~type")
+
+        p <- ggplot(traj.CI.area)+facet_wrap(~state, scales="free_y")
+        p <- p + geom_ribbon(data=traj.CI.area,aes(x=time,ymin=low,ymax=up,alpha=CI),fill="red")
+        p <- p + geom_line(data=traj.CI.line,aes(x=time,y=value,linetype=variable),colour="red")
+        p <- p + scale_alpha_manual("Percentile",values=c("95"=0.25,"50"=0.45),labels=c("95"="95th","50"="50th"))
+        p <- p + scale_linetype("Stats")
+        p <- p + guides(linetype = guide_legend(order = 1))
+
+    } else {
+
+        p <- ggplot(df.traj)+facet_wrap(~state, scales="free_y")
+        p <- p + geom_line(data=df.traj,aes(x=time,y=value,group=replicate),alpha=alpha,colour="red")
+
+    }
+
+    if(!is.null(data)){
+
+        # data are present, find which variable to plot by matching names of fitmodel states
+        data.obs.name <- intersect(names(data), state.names)
+        if(!length(data.obs.name)){
+            warnings("Names of ",sQuote("data")," don't match any state variable names of ",sQuote("traj"),": data won't be plotted.")
+        } else {
+            obs.name <- "observation"
+            names(obs.name) <- data.obs.name
+            data <- rename(data, obs.name)
+            data <- melt(data, measure.vars=obs.name,variable.name="state")
+
+            p <- p + geom_point(data=data,aes(x=time,y=value),colour="black")
+        }
+    }
+
     p <- p + theme_bw()
 
     if(plot){
@@ -37,10 +91,11 @@ plotModelTraj <- function(fitmodel,traj,state.variables=NULL,alpha=1, plot=TRUE)
 
 #'Plot fit of model to data
 #'
-#'Simulate the model under \code{theta}, generate observation and plot against data. Since simulation and observation processes can be stochastic, \code{n.replicates} can be plotted.
+#'This function simulates the model under \code{theta}, generates observation and plot them against the data. Since simulation and observation processes can be stochastic, \code{n.replicates} can be plotted.
 #' @param n.replicates numeric, number of replicated simulations.
+#' @param only.fit logical, if \code{TRUE} only the observations are plotted. Otherwise, all states are plotted.
 #' @inheritParams margLogLikeDeter
-#' @inheritParams plotModelTraj
+#' @inheritParams plotTraj
 #' @export
 #' @import plyr ggplot2 
 #' @return if \code{plot==FALSE}, a list of 2 elements is returned:
@@ -48,39 +103,25 @@ plotModelTraj <- function(fitmodel,traj,state.variables=NULL,alpha=1, plot=TRUE)
 #'     \item \code{simulations} \code{data.frame} of \code{n.replicates} simulated observations.
 #'     \item \code{plot} the plot of the fit.
 #' }
-plotThetaFit <- function(theta,fitmodel,n.replicates=1, alpha=min(1,10/n.replicates), plot=TRUE) {
+plotFit <- function(fitmodel, theta, state.init, data, n.replicates=1, summary=TRUE, alpha=min(1,10/n.replicates), only.fit=TRUE, plot=TRUE) {
 
-    if(is.null(fitmodel$data)){
-        stop(sQuote("fitmodel")," argument must have a ",sQuote("data"),call.=FALSE)
-    }
-
-    replicates <- 1:n.replicates
-    names(replicates) <- replicates
-
-    times <- c(0, fitmodel$data$time)
+    times <- c(0, data$time)
 
     cat("Simulate ",n.replicates," replicate(s)\n")
-    fit <- ldply(replicates,function(i) {
+    traj <- simulateModelReplicates(fitmodel=fitmodel,theta=theta, state.init=state.init, times=times, n=n.replicates, observation=TRUE)
 
-        # simulate model at successive observation times of data
-        traj <- fitmodel$simulate.model(theta,fitmodel$initialise.state(theta),times)
+    if(only.fit){
+        state.names <- c("observation")
+    } else {
+        state.names <- NULL
+    }
 
-        # generate observation
-        traj.obs <- fitmodel$generate.observation(traj,theta)
-
-        return(traj.obs)        
-
-    },.progress="text",.id="index")
-
-    p <- ggplot()
-    p <- p + geom_line(data=fit,aes(x=time,y=observation,group=index),alpha=alpha)
-    p <- p + geom_point(data=fitmodel$data,aes(x=time,y=Inc),colour="red")
-    p <- p + theme_bw()
+    p <- plotTraj(traj=traj, state.names=state.names, data=data, summary=summary, alpha=alpha, plot=FALSE)
 
     if(plot){
         print(p)        
     } else {
-        return(list(simulations=fit,plot=p))        
+        return(list(traj=traj,plot=p))        
     }
 
 }
@@ -90,23 +131,28 @@ plotThetaFit <- function(theta,fitmodel,n.replicates=1, alpha=min(1,10/n.replica
 #'
 #'Plot the observation generated by the filtered trajectories together with the data.
 #' @param smc output of \code{\link{particleFilter}}
-#' @inheritParams plotModelTraj
+#' @inheritParams plotTraj
+#' @inheritParams plotFit
 #' @export
 #' @import ggplot2 plyr
 #' @seealso particleFilter
-plotSMC <- function(smc,fitmodel,alpha=1,plot=TRUE) {
+plotSMC <- function(smc, fitmodel, theta, data=NULL, summary=TRUE, alpha=1, only.fit=TRUE, plot=TRUE) {
 
     traj <- smc$traj
     names(traj) <- 1:length(traj)
 
     traj <- ldply(traj,function(df) {
-        return(fitmodel$generate.observation(df,fitmodel$theta))
-    },.id="particle")
+        return(fitmodel$generate.observation(df,theta))
+    },.id="replicate")
 
-    p <- ggplot()
-    p <- p + geom_line(data=traj,aes(x=time,y=observation,group=particle),alpha=alpha)
-    p <- p + geom_point(data=fitmodel$data,aes(x=time,y=Inc),colour="red")
-    p <- p + theme_bw()
+    if(only.fit){
+        state.names <- c("observation")
+    } else {
+        state.names <- NULL
+    }
+
+    p <- plotTraj(traj=traj, state.names=state.names, data=data, summary=summary, alpha=alpha, plot=FALSE)
+
 
     if(plot){
         print(p)
@@ -148,7 +194,7 @@ plotTrace <- function(trace, estimated.only = FALSE){
 #' @export
 #' @import ggplot2 reshape2
 #' @seealso burnAndThin
-plotPosteriorDistribution <- function(trace, estimated.only = FALSE){
+plotPosteriorTheta <- function(trace, estimated.only = FALSE){
 
     if(estimated.only){
         is.fixed <- apply(trace,2,function(x) {length(unique(x))==1})
@@ -170,11 +216,12 @@ plotPosteriorDistribution <- function(trace, estimated.only = FALSE){
 #'Plot MCMC posterior fit
 #'
 #'Plot posterior distribution of observation generated under model's posterior parameter distribution.
-#' @param posterior.median logical, if \code{TRUE} use the median of the posterior distribution.
+#' @param posterior.summary character. Set to \code{"sample"} to plot trajectories from a sample of the posterior (default). Set to \code{"median"}, \code{"mean"} or \code{"max"} to plot trajectories corresponding to the median, mean and maximum of the posterior density.
 #' @param summary logical, if \code{TRUE} trajectories are summarised by their mean, median, 50\% and 95\% quantile distributions. Otheriwse, the trajectories are ploted.
-#' @param sample.size number of replicated simulations (if \code{posterior.median=TRUE}) or number of theta sampled from posterior distribution (if \code{posterior.median=TRUE}).
+#' @param sample.size number of theta sampled from posterior distribution (if \code{posterior.summary=="sample"}). Otherwise, number of replicated simulations.
+#' @inheritParams testFitmodel
 #' @inheritParams plotTrace
-#' @inheritParams plotModelTraj
+#' @inheritParams plotTraj
 #' @export
 #' @import ggplot2 plyr
 #' @return If \code{plot==FALSE}, a list of 2 elements is returned:
@@ -182,33 +229,42 @@ plotPosteriorDistribution <- function(trace, estimated.only = FALSE){
 #'    \item \code{posterior.traj} a \code{data.frame} with the trajectories (and observations) sampled from the posterior distribution.
 #'    \item \code{plot} the plot of the fit displayed.
 #'}
-plotPosteriorFit <- function(trace, fitmodel, posterior.median=FALSE, summary=FALSE, sample.size = 100, alpha=min(1,10/sample.size), plot=TRUE) {
+plotPosteriorFit <- function(trace, fitmodel, theta, state.init, posterior.summary=c("sample","median","mean","max"), summary=TRUE, sample.size = 100, alpha=min(1,10/sample.size), plot=TRUE) {
 
-    sample.size <- min(c(sample.size,nrow(trace)))
+    posterior.summary <- match.arg(posterior.summary)
 
-    index <- sample(1:nrow(trace),sample.size)
-    names(index) <- index
 
     # names of estimated theta
-    names.theta <- names(fitmodel$theta)
+    names.theta <- names(theta)
 
     # time sequence (must include initial time)
-    times <- c(0,fitmodel$data$time)
+    times <- c(0,data$time)
 
     message("Compute posterior fit")
 
-    if(posterior.median){
+    if(posterior.summary=="median"){
 
-        # theta.median <- apply(trace[names.theta],2,median)
-        # ind <- which.max(trace$log.posterior)
-        # theta.median <- trace[ind,names.theta]
-        theta <- c(R0=6.44,LP=1.36,IP=0.98,TIP=12,alpha=0.49,rho=0.65,pI0=4e-2,pL0=0.15,N=284)
-        fit <- simulateModelReplicates(fitmodel=fitmodel,theta=theta.median,times=times,n=sample.size,observation=TRUE)
-        fit <- rename(fit,c("replicate"="index"))
+        theta.median <- apply(trace[names.theta],2,median)
+        traj <- simulateModelReplicates(fitmodel=fitmodel,theta=theta.median,times=times,n=sample.size,observation=TRUE)
+
+    } else if(posterior.summary=="mean"){
+
+        theta.mean <- apply(trace[names.theta],2,mean)
+        traj <- simulateModelReplicates(fitmodel=fitmodel,theta=theta.mean,times=times,n=sample.size,observation=TRUE)
+
+    } else if(posterior.summary=="max"){
+        ind <- which.max(trace$log.posterior)
+        theta.max <- trace[ind,names.theta]
+        traj <- simulateModelReplicates(fitmodel=fitmodel,theta=theta.mean,times=times,n=sample.size,observation=TRUE)
 
     } else {
 
-        fit <- ldply(index,function(ind) {
+        sample.size <- min(c(sample.size,nrow(trace)))
+
+        index <- sample(1:nrow(trace), sample.size, replace=TRUE)
+        names(index) <- index
+
+        traj <- ldply(index,function(ind) {
 
             # extract posterior parameter set
             theta <- trace[ind,names.theta]
@@ -220,44 +276,18 @@ plotPosteriorFit <- function(trace, fitmodel, posterior.median=FALSE, summary=FA
             traj <- fitmodel$generate.observation(traj,theta)
 
             return(traj)
-        },.progress="text",.id="index")
+        },.progress="text",.id="replicate")
     }
 
-    if(summary){
-        message("Compute confidence intervals")
 
-        fit.CI <- ddply(fit,"time",function(df) {
-
-            tmp <- as.data.frame(t(quantile(df$observation,prob=c(0.025,0.25,0.5,0.75,0.975))))
-            names(tmp) <- c("low_95","low_50","median","up_50","up_95")
-            tmp$mean <- mean(df$observation)
-            return(tmp)
-
-        },.progress="text")
-
-        fit.CI.line <- melt(fit.CI[c("time","mean","median")],id.vars="time")
-        fit.CI.area <- melt(fit.CI[c("time","low_95","low_50","up_50","up_95")],id.vars="time")
-        fit.CI.area$type <- sapply(fit.CI.area$variable,function(x) {str_split(x,"_")[[1]][1]})
-        fit.CI.area$CI <- sapply(fit.CI.area$variable,function(x) {str_split(x,"_")[[1]][2]})
-        fit.CI.area$variable <- NULL
-        fit.CI.area <- dcast(fit.CI.area,"time+CI~type")
-
-        p <- ggplot()
-        p <- p + geom_ribbon(data=fit.CI.area,aes(x=time,ymin=low,ymax=up,alpha=CI),fill="red")
-        p <- p + geom_line(data=fit.CI.line,aes(x=time,y=value,linetype=variable),colour="red")
-        p <- p + geom_point(data=fitmodel$data,aes(x=time,y=Inc),colour="black")
-        p <- p + scale_alpha_manual("Confidence\ninterval",values=c("95"=0.25,"50"=0.45))
-        p <- p + scale_y_continuous("observation")    
-        p <- p + theme_bw()
-
+    if(only.fit){
+        state.names <- c("observation")
     } else {
+        state.names <- NULL
+    }
 
-        p <- ggplot()
-        p <- p + geom_line(data=fit,aes(x=time,y=observation,group=index),alpha=alpha,colour="red")
-        p <- p + geom_point(data=fitmodel$data,aes(x=time,y=Inc),colour="black")
-        p <- p + theme_bw()
+    p <- plotTraj(traj=traj, state.names=state.names, data=data, summary=summary, alpha=alpha, plot=FALSE)
 
-    }   
 
     if(plot){
         print(p)        
