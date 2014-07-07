7cd9c89daa671d981a6596a5836dcbbc4c098249
Anton <anton.camacho@lshtm.ac.uk>
2014-07-07 20:21:51 +0100

    ugly fix for plotESSBurn
    to plot as before when there is only one trace

--- R/plot.r
+++ R/plot.r
@@ -335,17 +335,20 @@ plotPosteriorFit <- function(trace, fitmodel, init.state, data, posterior.summar
 ##'
 ##' Takes an mcmc trace and tests the ESS at different values of burn-in
 ##' @param trace either a \code{data.frame} or a \code{list} of \code{data.frame} with all variables in column, as outputed by \code{\link{mcmcMH}}. Accept also \code{mcmc} or \code{mcmc.list} objects.
-##' @param longest.burn.in The longest burn in to test. Defaults to half the length of the trace
+##' @param longest.burn.in The longest burn-in to test. Defaults to half the length of the trace
 ##' @param step.size The size of the steps of burn-in to test. Defaults to 1/50th of \code{longest.burn.in}
 ##' @return a plot of the ESS against burn.in
 ##' @export
 ##' @import coda ggplot2 reshape2 plyr
-plotESSBurn <- function(trace, longest.burn.in = ifelse(length(trace)>1,nrow(trace[[1]]),nrow(trace)) / 2, step.size = round(longest.burn.in / 50)) {
+plotESSBurn <- function(trace, longest.burn.in = ifelse(is.data.frame(trace) | is.mcmc(trace),nrow(trace),nrow(trace[[1]])) / 2, step.size = round(longest.burn.in / 50)) {
 
     test.burn.in <- seq(0, longest.burn.in, step.size) # test values
 
     if(!class(trace)%in%c("mcmc.list","list")){
         trace <- list("chain1"=trace)
+        no_color <- TRUE
+    } else {
+        no_color  <- FALSE
     }
 
     if(is.null(names(trace))){
@@ -357,10 +360,10 @@ plotESSBurn <- function(trace, longest.burn.in = ifelse(length(trace)>1,nrow(tra
 # initialise data.frame of ess estimates
         ess.burn.in <- data.frame(t(effectiveSize(one.trace)))
         for (burn.in in test.burn.in[-1]) { 
-    # loop over all test values after 0
-        # test burn-in
+            # loop over all test values after 0
+            # test burn-in
             test.trace <- burnAndThin(one.trace, burn = burn.in)
-        # estimate ESS and at to vector of ess estimates
+            # estimate ESS and at to vector of ess estimates
             ess.burn.in <- rbind(ess.burn.in, t(effectiveSize(as.mcmc(test.trace))))
         }
         ess.burn.in$burn.in <- test.burn.in
@@ -371,9 +374,13 @@ plotESSBurn <- function(trace, longest.burn.in = ifelse(length(trace)>1,nrow(tra
 
     ess.long <- melt(df_ess.burn.in, id.vars = c("chain","burn.in"),value.name = "ESS", variable.name = "parameter")
 
-    p <- ggplot(ess.long, aes(x = burn.in, y = ESS, colour=chain))
+    p <- ggplot(ess.long, aes(x = burn.in, y = ESS))
     p <- p + facet_wrap(~ parameter)
-    p <- p + geom_line()
+    if(no_color){
+        p <- p + geom_line()
+    } else {
+        p <- p + geom_line(aes(color=chain))
+    }
     p <- p + theme_bw()
 
     print(p)
