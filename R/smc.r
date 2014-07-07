55358ac9bce1e04168cbcaaeeb1ca1f85ebf78ec
Anton <anton.camacho@lshtm.ac.uk>
2014-07-07 11:01:58 +0100

    minor fix

--- R/smc.r
+++ R/smc.r
@@ -21,7 +21,7 @@ particleFilter <- function(fitmodel, theta, init.state, data, n.particles, progr
 
     if(is.null(n.cores)){
         n.cores <- detectCores()
-        # cat("SMC runs on ",n.cores," cores\n")
+            # cat("SMC runs on ",n.cores," cores\n")
     }
 
     if(n.cores > 1){
@@ -106,81 +106,3 @@ particleFilter <- function(fitmodel, theta, init.state, data, n.particles, progr
     return(ans)
 
 }
-
-
-# my_bootstrapParticleFilter <- function(fitmodel, n.particles)
-# {
-
-#     ############################################################################################
-#     ## This function compute the marginal pointLogLike of the data (fitmodel$data)
-#     ## given the parameters (fitmodel$theta) using a particle filter
-#     ############################################################################################
-
-#     # Create some temporary variables (useful to avoid repetition of long names with $)
-#     data <- fitmodel$data
-#     theta <- fitmodel$theta
-
-#     ############################################################################################
-#     ## Initialisation of the algorithm
-#     ############################################################################################
-
-#     # Marginal log-likelihood is set to 0 and will be updated during the fitering steps
-#     pointLogLike <- 0
-
-#     # Initialise the particles:
-#     # initial state: vector calculated by the function fitmodel$initialise.state evaluated at theta
-#     initialise.particle  <- fitmodel$initialise.state(theta)
-
-#     # particle states can be stored in a list
-#     state.particles <- rep(list(initialise.particle),n.particles)
-#     # weight: initially equal for all the particles 
-#     # particle weight can be stored in a vector
-#     weight.particles <- rep(1/n.particles,length=n.particles)
-
-#     # initialise time variable
-#     current.time <- 0
-
-#     ############################################################################################
-#     # Loop over observation time: resample, propagate, weight
-#     ############################################################################################
-#     for(i in seq_len(nrow(data))){
-
-#         # extract next observtaion time from data
-#         next.time <- data$time[i]
-
-#         # resample particles according to their weight (normalization of the weight is done in the function sample())
-#         index.resampled <- sample(x=n.particles,size=n.particles,replace=T,prob=weight.particles)
-#         state.particles <- state.particles[index.resampled]
-
-#         # propagate particles to the next observation time
-#         for(p in 1:n.particles){
-
-#             # extract current state of the particle 
-#             current.state.particle <- unlist(state.particles[p])
-
-#             # simulate from current observation time to next observation time
-#             traj <- fitmodel$simulate(theta=theta,init.state=current.state.particle,times=c(current.time,next.time))
-
-#             # compute particle weight
-#             weight.particles[p] <- exp(fitmodel$pointLogLike( data=data, simu.traj=traj, theta= theta))
-
-#             # Update state of the p particle
-#             # You need to take last row of the traj data frame which corresponds to the next observation time
-#             # Also make sure to only take state variables (traj contains also a time variable)
-#             state.particles[[p]] <- traj[2,fitmodel$state.names]
-
-#         }
-
-#         # update time
-#         current.time <- next.time
-
-#         # Update marginal log-likelihood
-#         pointLogLike <- pointLogLike + log(mean(weight.particles))
-
-#     }
-
-
-#     # Return marginal log-likelihood
-#     return(pointLogLike)
-
-# }
