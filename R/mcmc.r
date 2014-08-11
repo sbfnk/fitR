89e4c2dc69e558074d056a345fd1cfd3e330e3dc
Sebastian Funk <sf7@princeton.edu>
2014-08-11 17:53:34 +0100

    moving average for acceptance rate

--- R/mcmc.r
+++ R/mcmc.r
@@ -9,8 +9,8 @@
 #' }
 #' @param init.theta named vector of initial parameter values to start the chain.
 #' @param proposal.sd vector of standard deviations. If this is given and covmat is not, a diagonal matrix will be built from this to use as covariance matrix of the multivariate Gaussian proposal distribution. By default, this is set to \code{init.theta/10}.
-#' @param covmat named numeric covariance matrix of the multivariate Gaussian proposal distribution. Must have named rows and columns with at least all estimated theta. If \code{proposal.sd} is given, this is ignored.
 #' @param n.iterations number of iterations to run the MCMC chain.
+#' @param covmat named numeric covariance matrix of the multivariate Gaussian proposal distribution. Must have named rows and columns with at least all estimated theta. If \code{proposal.sd} is given, this is ignored.
 #' @param limits limits for the - potentially truncated - multi-variate normal proposal distribution of the MCMC. Contains 2 elements:
 #' \itemize{
 #'      \item \code{lower} named numeric vector. Lower truncation points in each dimension of the Gaussian proposal distribution. By default they are set to \code{-Inf}.
@@ -22,6 +22,7 @@
 #' @param print.info.every frequency of information on the chain: acceptance rate and state of the chain. Default value to \code{n.iterations/100}. Set to \code{NULL} to avoid any info.
 #' @param verbose logical. If \code{TRUE}, information are printed.
 #' @param max.scaling.sd numeric. Maximum value for the scaling factor of the covariance matrix. Avoid too high values for the scaling factor, which might happen due to the exponential update scheme. In this case, the covariance matrix becomes too wide and the sampling from the truncated proposal kernel becomes highly inefficient
+#' @param acceptance.rate.weight if this is non-\code{NULL}, the acceptance rate is calculated as a moving average. This makes sure the acceptance rate is a reflection of recent proposals, and is useful for adaptive MCMC to prevent overshoot. If this is set to a value between 0 and 1, the acceptance rate will be calculated as \eqn{a = a_\mathrm{old} (1 - w) + a_\mathrm{new} w}, where \eqn{a_\mathrm{old}} is the (moving) average of the acceptance rate at the previous iteration, \eqn{a_\mathrm{new}} is 1 if the current proposal is accepted and 0 if it is rejected, and \eqn{w} is the value of \code{acceptance.rate.weight}. Values of \eqn{w} close to 1 give a lot of weight to recent proposals, while values closer to 0 give more weight to older proposals. A reasonable starting value is in the order of 1e-2.
 #' @note The size of the proposal covariance matrix is adapted using the following formulae: \deqn{\Sigma_{n+1}=\sigma_n * \Sigma_n} with \eqn{\sigma_n=\sigma_{n-1}*exp(\alpha^n*(acc - 0.234))},
 #' where \eqn{\alpha} is equal to \code{adapt.size.cooling} and \eqn{acc} is the acceptance rate of the chain.
 #'
@@ -43,8 +44,8 @@ mcmcMH <- function(target, init.theta, proposal.sd = NULL,
    adapt.size.start = NULL, adapt.size.cooling = 0.99,
    adapt.shape.start = NULL,
    print.info.every = n.iterations/100,
-   verbose = FALSE, max.scaling.sd = 50) {
-
+   verbose = FALSE, max.scaling.sd = 50,
+   acceptance.rate.weight = NULL) {
 
     # initialise theta
     theta.current <- init.theta
@@ -266,8 +267,17 @@ mcmcMH <- function(target, init.theta, proposal.sd = NULL,
         trace <- rbind(trace,c(target.theta.current$trace))
 
         # update acceptance rate
-        acceptance.rate <- acceptance.rate +
-        (is.accepted - acceptance.rate) / i.iteration
+        if (i.iteration == 1) {
+            acceptance.rate <- is.accepted
+        } else {
+            if (is.null(acceptance.rate.weight)) {
+                acceptance.rate <- acceptance.rate +
+                    (is.accepted - acceptance.rate) / i.iteration
+            } else {
+                acceptance.rate <- acceptance.rate * (1 - acceptance.rate.weight) +
+                    is.accepted * acceptance.rate.weight
+            }
+        }
 
         # update empirical covariance matrix
         tmp <- updateCovmat(covmat.empirical, theta.mean,
