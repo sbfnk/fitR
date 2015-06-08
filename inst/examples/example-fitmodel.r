e1e5d3675b9ff68489360379ab4cde18d6c04a39
Sebastian Funk <sbfnk@users.noreply.github.com>
2015-06-08 22:36:16 +0100

    indentation

--- inst/examples/example-fitmodel.r
+++ inst/examples/example-fitmodel.r
@@ -1,4 +1,4 @@
-# create a simple deterministic SIR model with constant population size
+## create a simple deterministic SIR model with constant population size
 
 SIR_name <- "SIR with constant population size"
 SIR_state.names <- c("S","I","R")
@@ -6,29 +6,33 @@ SIR_theta.names <- c("R0","D.inf")
 
 SIR_simulateDeterministic <- function(theta,init.state,times) {
 
-        SIR_ode <- function(time, state, parameters) {
+    SIR_ode <- function(time, state, parameters) {
 
-                ## parameters
-                beta <- parameters[["R0"]] / parameters[["D.inf"]]
-                nu <- 1 / parameters[["D.inf"]]
+        ## parameters
+        beta <- parameters[["R0"]] / parameters[["D.inf"]]
+        nu <- 1 / parameters[["D.inf"]]
 
-                ## states
-                S <- state[["S"]]
-                I <- state[["I"]]
-                R <- state[["R"]]
+        ## states
+        S <- state[["S"]]
+        I <- state[["I"]]
+        R <- state[["R"]]
 
-                N <- S + I + R
+        N <- S + I + R
 
-                dS <- -beta * S * I/N
-                dI <- beta * S * I/N - nu * I
-                dR <- nu * I
+        dS <- -beta * S * I/N
+        dI <- beta * S * I/N - nu * I
+        dR <- nu * I
 
-                return(list(c(dS, dI, dR)))
-        }
+        return(list(c(dS, dI, dR)))
+    }
 
-	trajectory <- data.frame(ode(y=init.state,times=times,func=SIR_ode,parms=theta, method = "ode45"))
+    trajectory <- data.frame(ode(y = init.state,
+                                 times = times,
+                                 func = SIR_ode,
+                                 parms = theta,
+                                 method = "ode45"))
 
-	return(trajectory)
+    return(trajectory)
 }
 
 ## function to compute log-prior
@@ -43,37 +47,36 @@ SIR_Prior <- function(theta, log = FALSE) {
 }
 
 ## function to compute the log-likelihood of one data point
-SIR_pointLogLike <- function(data.point, model.point, theta){
+SIR_pointLogLike <- function(data.point, model.point, theta, log = FALSE){
 
-        ## the prevalence is observed through a Poisson process
-	return(dpois(x=data.point[["obs"]], lambda=model.point[["I"]], log=TRUE))
+    ## the prevalence is observed through a Poisson process
+    return(dpois(x = data.point[["obs"]],
+                 lambda = model.point[["I"]],
+                 log = log))
 }
 
 ## function to generate observation from a model simulation
 SIR_genObsPoint <- function(model.point, theta){
 
-        ## the prevalence is observed through a Poisson process
-        obs.point <- rpois(n=1, lambda=model.point[["I"]])
+    ## the prevalence is observed through a Poisson process
+    obs.point <- rpois(n = 1, lambda = model.point[["I"]])
 
-        return(obs.point)
+    return(c(obs = obs.point))
 }
 
 ## create deterministic SIR fitmodel
 SIR <- fitmodel(
-	name=SIR_name,
-        state.names=SIR_state.names,
-	theta.names=SIR_theta.names,
-        simulate=SIR_simulateDeterministic,
-	genObsPoint=SIR_genObsPoint,
-	logPrior=SIR_logPrior,
-	pointLogLike=SIR_pointLogLike)
+    name = SIR_name,
+    state.names = SIR_state.names,
+    theta.names = SIR_theta.names,
+    simulate = SIR_simulateDeterministic,
     dprior = SIR_Prior,
+    rPointObs = SIR_genObsPoint,
+    dPointObs = SIR_pointLogLike)
+
+save(SIR, file = "SIR.rdata")
 
 ## test them
 theta <- c(R0 = 3, D.inf = 2)
 init.state <- c(S = 999, I = 1, R = 0)
 data(epi)
-
-## SIR
-## testFitmodel(fitmodel=SIR, theta=theta, init.state=init.state, data= epi1, verbose=TRUE)
-
